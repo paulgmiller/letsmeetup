191b922e3bab0b56e024aa381df4780d3db641f8
paul miller <pmiller@microsoft.com>
2019-08-25 21:45:02 -0700

    this seems to work

--- GuestsAPI.cs
+++ GuestsAPI.cs
@@ -15,8 +15,10 @@ namespace letsmeetup
     {
         public class Guest
         {
+            public string id { get => GuestId; }
             public string MeetupId { get; set; } = Guid.NewGuid().ToString("n");
             public string GuestId { get; set; } = Guid.NewGuid().ToString("n");
+            public string GuestName { get; set; }
             public double  Lat { get; set; }
             public double Long { get; set; }
         }
@@ -35,7 +37,7 @@ namespace letsmeetup
             ILogger log,
             string id)
         {
-            log.LogInformation("Getting guests for {id}");
+            log.LogInformation($"Getting guests for {id}");
             var newguest = new Guest();
             var guestcontainer = _dbClient.GetContainer("meetupdb", "guests");
             if (req.Method == "POST")
@@ -43,15 +45,16 @@ namespace letsmeetup
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 newguest = JsonConvert.DeserializeObject<Guest>(requestBody);
                 newguest.MeetupId = id;
-                log.LogInformation("adding guest {newguest.GuestId}");
-                await guestcontainer.CreateItemAsync<Guest>(newguest, new PartitionKey(newguest.MeetupId));
+                log.LogInformation($"adding/updating guest{newguest.GuestId}");
+                await guestcontainer.UpsertItemAsync<Guest>(newguest, new PartitionKey(newguest.MeetupId));
             }
             
-            var sqlQueryText = "SELECT * FROM c WHERE c.MeetupId = '{id}'";
+            var sqlQueryText = $"SELECT * FROM c WHERE c.MeetupId = '{id}'";
 
             log.LogInformation("Running query: {0}\n", sqlQueryText);
 
             var results = guestcontainer.GetItemQueryIterator<Guest>(new QueryDefinition(sqlQueryText));
+            //not sure how many this returns
             var guests = await results.ReadNextAsync();            
 
             return new OkObjectResult(guests);
